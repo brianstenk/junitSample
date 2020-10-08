29f410edd71802077a03427375d82f939f73eeff
Brian Nkurunungi <nkurunungib@gmail.com>
2020-10-07 20:43:49 -0500

    123

--- src/main/java/demo/example/utils/MathUtils.java
+++ src/main/java/demo/example/utils/MathUtils.java
@@ -1,4 +1,4 @@
-package io.javabrains.utils;
+package demo.example.utils;
 
 public class MathUtils {
 	
--- src/test/java/demo/example/utils/MathUtilsTest.java
+++ src/test/java/demo/example/utils/MathUtilsTest.java
@@ -1,4 +1,4 @@
-package io.javabrains.utils;
+package demo.example.utils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
